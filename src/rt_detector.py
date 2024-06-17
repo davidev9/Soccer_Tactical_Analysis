e0f837979c5c403942ced0a77957340ca65f92f7
davidev9 <vitaledavideu@gmail.com>
2024-06-17 13:42:20 +0200

    bug fixes

--- src/rt_detector.py
+++ src/rt_detector.py
@@ -11,7 +11,7 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.neighbors import NearestNeighbors
 
-model = YOLO("./src/yolo_weights/best.pt")
+model = YOLO("./yolo_weights/best.pt")
 
 class Detections:
     def __init__(self, xyxy, mask, confidence, class_id, tracker_id, data):
@@ -32,43 +32,27 @@ def extract_colors(image, xyxy):
     return np.array(colors)
 
 def create_clusters(detections, image):
-    # Step 1: Extract the colors from the bounding boxes
     colors = extract_colors(image, detections.xyxy)
-
-    # Step 2: Perform KMeans clustering based on colors
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(colors)  # Imposta n_clusters a 2 per due team
+    kmeans = KMeans(n_clusters=2, random_state=0).fit(colors)
     labels = kmeans.labels_
-
-    # Step 3: Add the team attribute
     team_labels = np.where(labels == 0, 'TA', 'TB')
     detections.data['team'] = team_labels
-
-    # Step 4: Find the instance with the maximum distance from both cluster centers
-    distances_to_centers = kmeans.transform(colors)  # Calcola le distanze tra i colori e i centroidi dei cluster
-    max_distances = np.max(distances_to_centers, axis=1)  # Trova le massime distanze per ciascun colore
+    distances_to_centers = kmeans.transform(colors) 
+    max_distances = np.max(distances_to_centers, axis=1)
 
     # Trova l'istanza con la massima distanza sia dal primo che dal secondo cluster
     max_distance_index = np.argmax(max_distances)
-
     detections.data['team'][max_distance_index] = 'ref'
-    # Store the cluster centers for future use
     detections.data['cluster_centers'] = kmeans.cluster_centers_
 
     return detections
 
 def assign_teams(detections, cluster_centers, image, threshold=30):
-    # Step 1: Extract the colors from the bounding boxes
     colors = extract_colors(image, detections.xyxy)
-
-    # Step 2: Use Nearest Neighbors to find the closest cluster center
     nbrs = NearestNeighbors(n_neighbors=1).fit(cluster_centers)
     distances, indices = nbrs.kneighbors(colors)
-
-    # Step 3: Add the team attribute
     team_labels = np.where(indices.flatten() == 0, 'TA', 'TB')
     detections.data['team'] = team_labels
-
-    # Step 4: Find the instances with distances beyond the threshold and assign them as 'ref'
     outlier_indices = np.where(np.max(distances, axis=1) > threshold)[0]
     detections.data['team'][outlier_indices] = 'ref'
 
@@ -121,7 +105,7 @@ def process_frame(frame: np.ndarray, _) -> np.ndarray:
     if i==0:
       det2=detections
 
-    if i<15:
+    if i<10:
       det2=merge_detections(det2,detections)
       detections_with_clusters = create_clusters(det2,frame.copy())
       cluster_centers = detections_with_clusters.data['cluster_centers']
@@ -135,7 +119,7 @@ def process_frame(frame: np.ndarray, _) -> np.ndarray:
     print(i)
     # Crea le etichette usando i valori estratti
     labels = [
-    f"#{tracker_id} {team}" if class_id == 1 and team != "ref" else f"#{tracker_id}"
+    f"#{tracker_id} {team}" if class_id == 1 and (team == "TA" or team == "TB") else f"#{tracker_id}"
     for tracker_id, class_id, team in zip(detections.tracker_id, detections.class_id, detections.data['team'])
     ]
 
@@ -149,10 +133,9 @@ def process_frame(frame: np.ndarray, _) -> np.ndarray:
 
     label_annotator = sv.LabelAnnotator(color=sv.Color.BLACK)
 
-    annotated_frame = label_annotator1.annotate(
-        scene=annotated_frame, detections=detections, labels=team_A_labels)
-    annotated_frame = label_annotator2.annotate(
-        scene=annotated_frame, detections=detections, labels=team_B_labels)
+    annotated_frame = label_annotator.annotate(
+        scene=annotated_frame, detections=detections, labels=labels)
+
     #detections = detections[detections.class_id == 1]
     #annotated_frame = trace_annotator.annotate(
     #       scene=annotated_frame,
@@ -165,7 +148,7 @@ if __name__ == "__main__":
     HOME = os.getcwd()
     print(HOME)
     
-    VIDEO_PATH="./src/videos/challenge-1179_1.mp4"
+    VIDEO_PATH="./videos/challenge-1179_1.mp4"
     file_name_with_extension = os.path.basename(VIDEO_PATH)
     videoname = os.path.splitext(file_name_with_extension)[0]
     box_annotator = sv.BoundingBoxAnnotator(thickness=1)
