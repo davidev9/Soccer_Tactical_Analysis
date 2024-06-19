96fa273aecf5920c82b05b82dcd86dd4ae3c1473
davidev9 <vitaledavideu@gmail.com>
2024-06-19 19:27:03 +0200

    bug fixes

--- src/rt_player_tracker.py
+++ src/rt_player_tracker.py
@@ -44,7 +44,8 @@ coordinate = {
 }
 
 def pitch_estimator(field_detections):
-    id_array=field_detections.class_id
+    name_array = field_detections.data['class_name']
+    id_array = [int(x.strip("'")) for x in name_array]
     num_keypoint=len(id_array)
     points_array = field_detections.get_anchors_coordinates(
                 anchor=sv.Position.BOTTOM_CENTER
@@ -83,37 +84,44 @@ class ViewTransformer:
 
 
 
+
+
 def filter_detections(detections):
     # Trova gli indici delle prime detections per ogni class_id
     _, unique_indices = np.unique(detections.class_id, return_index=True)
-
     # Filtra le detections basate sugli indici trovati
     filtered_xyxy = detections.xyxy[unique_indices]
     filtered_class_id = detections.class_id[unique_indices]
     filtered_confidence = detections.confidence[unique_indices]
 
-    # Se ci sono tracker_id e data, filtrarli in base agli stessi indici
+    sorted_indices = np.argsort(filtered_confidence)[::-1]
+    filtered_xyxy = filtered_xyxy[sorted_indices]
+    filtered_class_id = filtered_class_id[sorted_indices]
+    filtered_confidence = filtered_confidence[sorted_indices]
+
     filtered_tracker_id = None
     filtered_data = None
     if detections.tracker_id is not None:
-        filtered_tracker_id = detections.tracker_id[unique_indices]
+        filtered_tracker_id = detections.tracker_id[unique_indices][sorted_indices]
     if detections.data is not None:
-        filtered_data = {key: detections.data[key][unique_indices] for key in detections.data}
+        filtered_data = {key: detections.data[key][unique_indices][sorted_indices] for key in detections.data}
 
-    # Creare un nuovo oggetto Detections con le detections filtrate
-    filtered_detections = sv.Detections(filtered_xyxy,None, filtered_confidence,filtered_class_id,
-                                     tracker_id=filtered_tracker_id, data=filtered_data)
+    filtered_detections = sv.Detections(filtered_xyxy, None, filtered_confidence, filtered_class_id,
+                                        tracker_id=filtered_tracker_id, data=filtered_data)
+    print("filtered detections:", filtered_detections)
     return filtered_detections
 
 
 
-def process_frame(frame: np.ndarray, _) -> np.ndarray:
 
+
+def process_frame(frame: np.ndarray, _) -> np.ndarray:
+    global source,target
     ellipse_annotator = sv.EllipseAnnotator(color=sv.Color.YELLOW,thickness=1,start_angle=0,end_angle=200)
     circle_annotator = sv.CircleAnnotator(color=sv.Color.YELLOW,thickness=1)
 
     results = player_model(frame, imgsz=1280,conf=0.1)[0]
-    results2=field_model(frame,imgsz=1280,conf=0.01)[0]
+    results2=field_model(frame,imgsz=1280,conf=0.075)[0]
 
     detections = sv.Detections.from_ultralytics(results)
     detections = tracker.update_with_detections(detections).with_nms(threshold=0.1)
@@ -122,24 +130,30 @@ def process_frame(frame: np.ndarray, _) -> np.ndarray:
     detections2= sv.Detections.from_ultralytics(results2)
     detections2=filter_detections(detections2)
     
-    source,target=pitch_estimator(detections2)
-    
-    view_transformer = ViewTransformer(source=source, target=target)
+    new_source,new_target=pitch_estimator(detections2)
+    if len(new_source) > 0:
+        source=new_source
+        target=new_target
 
-    points = detections.get_anchors_coordinates(
-                anchor=sv.Position.BOTTOM_CENTER
-        )
-    points = view_transformer.transform_points(points=points).astype(int)
-    #print(points)
-
-    labels=[]
-    #Label con le coordinate reali
-    for j, detection in enumerate(detections):
-        label=f"{detection[4]} xy:({points[j]})"
-        labels.append(label)
+    annotated_frame=frame.copy()
+    if len(source) > 0:
+        view_transformer = ViewTransformer(source=source, target=target)
+        points = detections.get_anchors_coordinates(
+                    anchor=sv.Position.BOTTOM_CENTER
+            )
+        points = view_transformer.transform_points(points=points).astype(int)
+        labels=[]
+        #Label con le coordinate reali
+        for j, detection in enumerate(detections):
+            label=f"{detection[4]} xy:({points[j]})"
+            labels.append(label)
+            
+        annotated_frame = label_annotator.annotate(
+        scene=annotated_frame, detections=detections, labels=labels)      
+        
 
     annotated_frame = circle_annotator.annotate(
-    scene=frame.copy(),
+    scene=annotated_frame,
     detections=detections2,
     )
 
@@ -148,20 +162,21 @@ def process_frame(frame: np.ndarray, _) -> np.ndarray:
     detections=detections,
     )
 
-
-    annotated_frame = label_annotator.annotate(
-        scene=annotated_frame, detections=detections, labels=labels)
     return annotated_frame
+
+
 
 if __name__ == "__main__":
     HOME = os.getcwd()
     print(HOME)
     player_model = YOLO("./yolo_weights/best.pt")
-    field_model = YOLO("./yolo_weights/keypoint_field.pt")
+    field_model = YOLO("./yolo_weights/keypoint_field_v2.pt")
     VIDEO_PATH="./videos/challenge-1179_1.mp4"
     file_name_with_extension = os.path.basename(VIDEO_PATH)
     videoname = os.path.splitext(file_name_with_extension)[0]
     tracker = sv.ByteTrack()
     label_annotator = sv.LabelAnnotator(color=sv.Color.BLUE,text_padding=5)
     ellipse_annotator = sv.EllipseAnnotator(color=sv.Color.BLUE,thickness=1,start_angle=0,end_angle=200)
+    source=[]
+    target=[]
     sv.process_video(source_path=VIDEO_PATH, target_path=f"result.mp4", callback=process_frame)
