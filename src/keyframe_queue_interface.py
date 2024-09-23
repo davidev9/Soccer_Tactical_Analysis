cf53c9816e57105473841e99dcf52c7bf2633521
davidev9 <101256668+davidev9@users.noreply.github.com>
2024-09-23 15:29:34 +0200

    Add files via upload

--- src/keyframe_queue_interface.py
+++ src/keyframe_queue_interface.py
@@ -6,21 +6,24 @@ import json
 import matplotlib.pyplot as plt
 import os
 class Keyframe:
-    def __init__(self, image, video_name, minute, second, tracker_data):
+    def __init__(self, image, raw_image, video_name, minute, second, tracker_data):
         self.image = image
+        self.raw_image = raw_image  
         self.video_name = video_name
         self.minute = minute
         self.second = second
         self.tracker_data = tracker_data
 
     def to_redis_format(self) -> bytes:
-        image_bytes = self.serialize_image()
+        image_bytes = self.serialize_image(self.image)
+        raw_image_bytes = self.serialize_image(self.raw_image)  
         data = {
             'video_name': self.video_name,
             'minute': self.minute,
             'second': self.second,
             'tracker_data': self.tracker_data,
-            'image': image_bytes
+            'image': image_bytes,
+            'raw_image': raw_image_bytes  
         }
         return json.dumps(data).encode()
 
@@ -29,16 +32,16 @@ class Keyframe:
         try:
             data = json.loads(serialized_data)
             image = cls.deserialize_image(data['image'])
-            return cls(image, data['video_name'], data['minute'], data['second'], data['tracker_data'])
+            raw_image = cls.deserialize_image(data['raw_image'])  # Deserializziamo raw_image
+            return cls(image, raw_image, data['video_name'], data['minute'], data['second'], data['tracker_data'])
         except (json.JSONDecodeError, KeyError) as e:
             print(f"Error decoding data: {e}")
             print(f"Serialized data causing error: {serialized_data}")
             return None
 
-
-    def serialize_image(self) -> str:
+    def serialize_image(self, img: Image) -> str:
         with BytesIO() as output:
-            self.image.save(output, format='PNG')
+            img.save(output, format='PNG')
             image_bytes = output.getvalue()
         return base64.b64encode(image_bytes).decode('utf-8')
 
@@ -59,8 +62,6 @@ class KeyframeQueue:
     def BU_list_len(self) -> int:
         return self._r.llen("keyframes_backup")
 
-
-
     def push_keyframe(self, keyframe: Keyframe) -> int:
         serialized_keyframe = keyframe.to_redis_format()
         return self._r.lpush("keyframes", serialized_keyframe)
@@ -78,7 +79,6 @@ class KeyframeQueue:
         if not serialized_keyframe:
             return (None, None)
 
-
         keyframe = Keyframe.from_redis_format(serialized_keyframe[0])
         return serialized_keyframe, keyframe
 
@@ -88,36 +88,7 @@ class KeyframeQueue:
     def delete_main(self):
         self._r.delete("keyframes")
 
-
     def del_keyframe_from_backup(self, keyframe_ref: bytes):
         if keyframe_ref:
             self._r.lrem("keyframes_backup", 1, keyframe_ref)
 
-
-def main():
-    redis_conn = Redis(
-        host=os.getenv("REDIS_HOST"),
-        port=os.getenv("REDIS_PORT"),
-        password=os.getenv("REDIS_PASSWORD"),
-        decode_responses=True
-    )
-    
-    # Initializing the keyframe queue
-    kf_queue = KeyframeQueue(redis_conn)
-    ciao=kf_queue.list_len()
-    print("coda princ",ciao)
-    keyframe_ref, retrieved_keyframe = kf_queue.pop_keyframe()
-    img_frame=retrieved_keyframe.image
-    plt.imshow(img_frame)
-    plt.axis('off')  # Nascondi gli assi
-    plt.show()
-    ciao=kf_queue.BU_list_len()
-    print("coda back prima",ciao)
-    kf_queue.del_keyframe_from_backup(keyframe_ref)
-    ciao=kf_queue.BU_list_len()
-    print("coda back dopo",ciao)
-
-
-
-if __name__ == "__main__":
-    main()
